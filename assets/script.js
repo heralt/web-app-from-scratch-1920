7db15ccd18b92767b03aaf1b78b80891b6bd3486
heralt <heralt@live.nl>
2020-02-07 10:12:45 +0100

    formatted my code

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,35 @@
-
 const requestLink = 'https://ghibliapi.herokuapp.com/films';
 const mainContainer = document.getElementById("myData");
 
 fetch(requestLink)
-.then(response => {
-    if(response.ok){
-        console.log("SUCCESS");
-    } else {
-        console.log("FAILED")
-    }
-    return response.json(); // returns promise object
-})
-.then((json) => {
-    getWantedData(json)
-})
-    .catch( err => {
+    .then(response => {
+        if (response.ok) {
+            console.log("SUCCESS");
+        } else {
+            console.log("FAILED")
+        }
+        return response.json(); // returns promise object
+    })
+    .then((json) => {
+        getWantedData(json)
+    })
+    .catch(err => {
         console.log(err);
     });
 
-function getWantedData(data){
+function getWantedData(data) {
     const cleanData = data.map((item) => {
-       return  {
-           title: item.title,
-           desc: item.description
-       }
+        return {
+            title: item.title,
+            desc: item.description
+        }
     });
     display(cleanData)
 }
 
-function display(wantedData){
+function display(wantedData) {
 
-    for(let i = 0; i < wantedData.length; i++){
+    for (let i = 0; i < wantedData.length; i++) {
         let div = document.createElement("div");
         div.innerHTML = 'movies: ' + wantedData[i].title;
         mainContainer.appendChild(div);
